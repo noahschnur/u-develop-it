97762bb1b091e4c4488140b9da4f1a6bca5dd28c
Noah <noahschnur@gmail.com>
2021-04-18 15:04:17 -0700

    start 12.2

--- db/schema.sql
+++ db/schema.sql
@@ -1,20 +1,6 @@
 CREATE TABLE candidates (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR (30) NOT NULL,
-    last_name VARCHAR (30) NOT NULL,
-    industry_connected BOOLEAN NOT NULL
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  industry_connected BOOLEAN NOT NULL
 );
-
-
-
--- SELECT * FROM candidates;
-
--- SELECT first_name, last_name FROM candidates;
-
--- SELECT first_name, industry_connected
--- FROM candidates
--- WHERE industry_connected = 1;
-
--- SELECT first_name, last_name, industry_connected
--- FROM candidates
--- WHERE id = 5;
