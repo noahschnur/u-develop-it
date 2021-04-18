509c512960cc5d84e79a29265301ffd6672e7c03
Noah <noahschnur@gmail.com>
2021-04-18 16:05:00 -0700

    add sql to server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const e = require('express');
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,10 +8,25 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'IloveBrittany5!',
+        database: 'election'
+    },
+    console.log('connected to election database')
+);
+
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello'
     });
+});
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
 
 // default response for any other request (Not Found)
